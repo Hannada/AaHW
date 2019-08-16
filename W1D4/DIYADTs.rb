49a32cfebf5bd7a36488d4f5e68dcd3813a988e2
Hannada <dphanna@gmail.com>
2019-08-15 22:34:24 -0400

    First attempt

---
+++ W1D4/DIYADTs.rb
@@ -0,0 +1,33 @@
+class Stack
+    def initialize
+        # create ivar to store stack here!
+        @ivar = ivar 
+    end
+
+    def push(el)
+        # adds an element to the stack
+        self.push(el)
+    end
+
+    def pop
+        # removes one element from the stack
+        self.pop(el)
+    end
+
+    def peek
+        # returns, but doesn't remove, the top element in the stack
+        self.last 
+    end
+end
+
+class Queue 
+    def enqueue(el)
+    end
+
+    def dequeue
+    end
+
+    def peek
+    end
+    
+end
