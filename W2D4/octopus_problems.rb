fe8774d72f02aebe8a56f4fdebb05113711fa455
Hannada <dphanna@gmail.com>
2019-08-21 22:59:33 -0400

    Octo

---
+++ W2D4/octopus_problems.rb
@@ -0,0 +1,17 @@
+FISH = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
+
+def sluggish_octopus
+    
+end
+
+def dominant_octopus
+
+end
+
+def clever_octopus
+
+end
+
+def sluggish_octopus
+
+end
