1d2a4279b27a6b0e9dc1e7fa343aa1bbae1c9c53
Hannada <dphanna@gmail.com>
2019-09-12 00:41:44 -0400

    HW

---
+++ W5D4/intro.js
@@ -0,0 +1,14 @@
+function mysteryScoping1() {
+    var x = 'out of block';
+    if (true) {
+      var x = 'in block';  
+      console.log(x);
+    }
+    console.log(x);
+  }
+
+function madLib (arg1, arg2, arg3) {
+    return arg1.prototycpe.toUpperCase;
+}  
+
+madLib('make', 'best', 'guac');
